5de342043e727d772f695bd11f3a48def3d22ecc
sarawarnock <sara@sarawarnock.com>
2020-11-14 16:48:31 -0700

    start adding js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//store all 
+//store all DOM elements as attributes inside of an object for DRY (do not repeat yourself)
+//then access all DOM elements by calling properties from this object
+
+function getTilesDom() {
+    return Array.from(document.querySelectorAll("td"));
+}
+
+const DOM = {
+    tiles: getTilesDom(),
+    x: 'blue', //image
+    o: 'green', //image
+    alert: document.querySelector(".alert"),
+    boardSizeInput: document.querySelector("#board-size-input"),
+    toWinInput: document.querySelector("#to-win-input"),
+    submitButton: document.querySelector("#submit-button"),
+    board: document.querySelector("tbody"),
+};
+
